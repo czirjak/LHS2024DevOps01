630ccd23bbd50cefc481242b173cd31fbd9bd137
BpeterDEVOPS <bors.peter15@gmail.com>
2024-11-18 12:18:31 +0100

    Update v1-01-backend.tf

--- infrastructure/03-infrastructure/v1-01-backend.tf
+++ infrastructure/03-infrastructure/v1-01-backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name       = "codehunters-storages"
-    storage_account_name      = " TODO "
-    container_name            = " TODO "
+    storage_account_name      = "codehuntersstorageacc"
+    container_name            = "blobcontainers"
     key                       = "codehunters.training.terraform.tfstate"
   }
 }
